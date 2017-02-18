e6bc4db9582aa0a823c6d601ea527853bab2b65b
Daniel Rodriguez <daniel.rguez.hdez@gmail.com>
2017-02-18 20:19:32 +0100

    Fix logger attribute

--- src/main/java/com/drodriguezhdez/sandbox/akka/lesson1/Lesson1App.java
+++ src/main/java/com/drodriguezhdez/sandbox/akka/lesson1/Lesson1App.java
@@ -1,15 +1,19 @@
 package com.drodriguezhdez.sandbox.akka.lesson1;
 
+import java.util.logging.Logger;
+
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
 
 public class Lesson1App {
+	
+	private static final Logger logger = Logger.getAnonymousLogger();
 
 	public static void main(String[] args) {
 		final ActorSystem actorSystem = ActorSystem.create("actorSystem");
 		
-		final ActorRef myActorRef = actorSystem.actorOf(Props.create(MyActor.class));
+		final ActorRef myActorRef = actorSystem.actorOf(Props.create(MyActor.class, logger));
 		myActorRef.tell("Dani", ActorRef.noSender());
 		
 	}
